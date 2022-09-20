f4bb5736166f51a18888dbd904caee94c7d2a02c
teixeirazeus <teixeira.zeus@gmail.com>
2022-09-20 11:44:09 -0300

    comment

--- aiocorreios/data_util.py
+++ aiocorreios/data_util.py
@@ -3,4 +3,7 @@ from decimal import Decimal
 class DataUtil:
     @staticmethod
     def to_decimal(data):
+        """
+        Parse the data to decimal.
+        """
         return Decimal(data.replace(',', '.'))
